98d38fb96ff8f27f78602aebb7223f98b0fab7f2
shanoo <shanoo.kumar11@gmail.com>
2025-06-10 19:02:17 +0530

    complete code

--- .history/backend/index_20250610183732.js
+++ .history/backend/index_20250610183732.js
@@ -35,7 +35,7 @@ app.use("/api/v1/application", applicationRoute);
 
 
 app.use(express.static(path.join(_dirname, "/frontend/dist")));
-app.get('*', (_,res)=>{
+app.get('*',(_,res)=>{
    res.sendFile(path.resolve(_dirname,"frontend","dist","index.html")); 
 })
 
--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -1,25 +1,4 @@
 
-
-// import axios from "axios";
-
-// export const createAxiosInstance = (token) => {
-//   const instance = axios.create({
-//     baseURL: "http://localhost:8000/api/v1",
-//     withCredentials: true,
-//   });
-
-//   instance.interceptors.request.use((config) => {
-//     if (token) {
-//       config.headers.Authorization = `Bearer ${token}`;
-//     }
-//     return config;
-//   });
-
-//   return instance;
-// };
-
-
-// utils/createAxiosInstance.js
 import axios from "axios";
 
 function getCookie(name) {
@@ -30,10 +9,10 @@ function getCookie(name) {
 }
 
 export const createAxiosInstance = () => {
-  const token = getCookie("token"); // 👈 directly read from cookie
+  const token = getCookie("token"); 
 
   const instance = axios.create({
-    baseURL: "http://localhost:8000/api/v1",
+    baseURL: import.meta.env.VITE_API_BASE_URL,
     withCredentials: true,
   });
 
