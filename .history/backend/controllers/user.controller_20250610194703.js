2a77920f32ebfc3be719a2e555f7144f905cac1d
shanoo <shanoo.kumar11@gmail.com>
2025-06-10 19:50:01 +0530

    update

--- .history/backend/controllers/user.controller_20250610194703.js
+++ .history/backend/controllers/user.controller_20250610194703.js
@@ -95,7 +95,7 @@ export const login = async (req, res) => {
       profile: user.profile
     }
 
-    return res.status(200).cookie("token", token, { maxAge: 1 * 24 * 60 * 60 * 1000, httpOnly: true, secure: true,  sameSite: 'None' }).json({
+    return res.status(200).cookie("token", token, { maxAge: 1 * 24 * 60 * 60 * 1000, httpOnly: true, sameSite: 'None' }).json({
       message: `Welcome back ${user.fullname}`,
       user,
       success: true
